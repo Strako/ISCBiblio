190f9387556f1671bf78c4c6fd81a56cc003f51a
Armando Ibarra <70788529+Strako@users.noreply.github.com>
2023-02-01 22:00:25 -0600

    added logs table
    Added logs table to the db

--- DB/ISCBiblio.sql
+++ DB/ISCBiblio.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: ISCBiblio
 -- ------------------------------------------------------
--- Server version	8.0.32-0buntu0.22.04.1
+-- Server version	8.0.32-0ubuntu0.22.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -102,6 +102,29 @@ LOCK TABLES `Clients` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `Logs`
+--
+
+DROP TABLE IF EXISTS `Logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Logs` (
+  `LogID` int NOT NULL AUTO_INCREMENT,
+  `Log` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `LogID` (`LogID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Logs`
+--
+
+LOCK TABLES `Logs` WRITE;
+/*!40000 ALTER TABLE `Logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Users`
 --
 
@@ -114,7 +137,7 @@ CREATE TABLE `Users` (
   `Password` varchar(255) NOT NULL,
   PRIMARY KEY (`UserID`),
   UNIQUE KEY `UserID` (`UserID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,6 +146,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'user@mail.com','1234567');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -135,4 +159,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-28 18:23:28
+-- Dump completed on 2023-02-01 21:56:43
