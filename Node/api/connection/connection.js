eac2c19a767ab177ac56ccb96493c121b865a5f6
Armando Ibarra <70788529+Strako@users.noreply.github.com>
2023-02-11 19:32:45 -0600

    updated DB diagram and readme

--- Node/api/connection/connection.js
+++ Node/api/connection/connection.js
@@ -3,7 +3,7 @@ const mysqlConnection = mysql.createConnection(
 {
 host:'localhost',
 user: 'root',
-password: 'your own password',
+password: 'Your own password',
 database: 'ISCBiblio',
 port: '3306'
 });
